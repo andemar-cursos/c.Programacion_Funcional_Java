6f6ccce0e2428e574c0ba089a637ab8282c3c3e5
Anderson <andemartinezdev@gmail.com>
2021-06-29 11:36:05 -0500

    03-04 -> Predicate
    - Se uso el predicate, el cual es una Function pero su retorno siempre sera boolean.

--- src/main/java/com/andemar/cursos/functional/math/MathFunction.java
+++ src/main/java/com/andemar/cursos/functional/math/MathFunction.java
@@ -1,10 +1,13 @@
 package com.andemar.cursos.functional.math;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class MathFunction  {
 
     public static void main(String[] args) {
+
+        // Ejemplo 1 ******************************
 
         Function<Integer, Integer> square = new Function<Integer, Integer>() {
             @Override
@@ -13,7 +16,41 @@ public class MathFunction  {
             }
         };
 
-        System.out.println(square.apply(5));
-        System.out.println(square.apply(10));
+//        System.out.println(square.apply(5));
+//        System.out.println(square.apply(10));
+
+
+        // Ejemplo 2 ******************************
+
+        // Revisa si es un par
+        Function<Integer, Boolean> isOdd = x -> x % 2 == 1;
+//        System.out.println( isOdd.apply(4) );
+
+        // Revisa si es un par
+        // Predicate siempre retorna un boolean
+        Predicate<Integer> isEven = x -> x % 2 == 0;
+//        System.out.println( isEven.test(4) );
+
+
+        // Ejemplo 3 ******************************
+
+        Predicate<Student> aprobo = student -> student.getCalificacion() >= 6;
+
+        System.out.println(aprobo.test(new Student(5.0)));
     }
+
+
+    static class Student {
+
+        private double calificacion;
+
+        public Student(double calificacion) {
+            this.calificacion = calificacion;
+        }
+
+        public double getCalificacion() {
+            return calificacion;
+        }
+    }
+
 }
