5add0596c0dc41ea46670e1bd946cbd328993fe1
Anderson <andemartinezdev@gmail.com>
2021-06-29 15:32:11 -0500

    03-11 ->Chaining
    - Bug

--- src/main/java/com/andemar/cursos/functional/chaining/Chaining.java
+++ src/main/java/com/andemar/cursos/functional/chaining/Chaining.java
@@ -1,2 +1,31 @@
-package com.andemar.cursos.functional.chainig;public class Chaining {
+package com.andemar.cursos.functional.chaining;
+
+public class Chaining {
+
+    public static void main(String[] args) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("ola")
+                     .append("q")
+                     .append("ace");
+
+        Chainer chainer = new Chainer();
+
+        chainer .sayHi()
+                .sayBye();
+    }
+
+    static class Chainer {
+
+        public Chainer sayHi() {
+            System.out.println("Hola");
+            return this;
+        }
+
+        public Chainer sayBye() {
+            System.out.println("Adios");
+            return this;
+        }
+    }
 }
